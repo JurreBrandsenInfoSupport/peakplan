887b70bea13ceff86515a3bd0f1944484e05699f
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-04-27 18:20:51 +0000

    Add missing validations

--- backend/app/models/project.rb
+++ backend/app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ApplicationRecord
+    has_many :tasks
+    validates :owner, presence: true
 end
--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -1,8 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :project, optional: true
   validates :title, presence: true
-
+  validates :owner, presence: true
   attribute :done, :boolean, default: false
-
-  # Attributes: title:string, description:text, deadline:datetime, done:boolean
 end
