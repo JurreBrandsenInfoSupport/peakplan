4696d0a7b20dcc8db033dbf34bcd8202516f0bc8
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-05-01 13:38:19 +0200

    Improve api handling

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,84 @@
+import {
+  ProjectDetails,
+  ProjectSummary,
+  TaskResponseData as TaskListData,
+} from "./types";
+
+const backendUrl = "http://localhost:3000";
+
+export class ApiError extends Error {
+  status: number;
+
+  constructor(message: string, status: number) {
+    super(message);
+    this.status = status;
+    Object.setPrototypeOf(this, ApiError.prototype);
+  }
+}
+
+function getApiHost() {
+  if (typeof window !== "undefined") {
+    return "/api";
+  }
+
+  return backendUrl;
+}
+
+async function processResponse<T>(response: Response): Promise<T> {
+  if (response.status === 404) {
+    throw new ApiError("Not found", 404);
+  }
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new ApiError(
+      `Error: ${response.status} ${response.statusText} - ${errorText}`,
+      response.status
+    );
+  }
+
+  return await response.json();
+}
+
+export async function fetchProject(id: string): Promise<ProjectDetails> {
+  return fetch(`${getApiHost()}/projects/${id}`).then(
+    processResponse<ProjectDetails>
+  );
+}
+
+export async function fetchProjects(): Promise<ProjectSummary[]> {
+  return fetch(`${getApiHost()}/projects`).then(
+    processResponse<ProjectSummary[]>
+  );
+}
+
+export async function fetchProjectTasks(
+  projectId: number
+): Promise<TaskListData> {
+  return fetch(`${getApiHost()}/projects/${projectId}/tasks`).then(
+    processResponse<TaskListData>
+  );
+}
+
+export async function createProjectTask(
+  projectId: number,
+  data: { title: string; description: string; deadline?: Date }
+) {
+  return fetch(`${getApiHost()}/projects/${projectId}/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(processResponse);
+}
+
+export async function updateTaskStatus(taskId: number, done: boolean) {
+  return fetch(`${getApiHost()}/tasks/${taskId}`, {
+    method: "PATCH",
+    body: JSON.stringify({ done }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -8,3 +8,16 @@ export interface ProjectDetails {
   title: string;
   description: string;
 }
+
+export interface TaskResponseData {
+  completed: Task[];
+  pending: Task[];
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  done: boolean;
+  deadline?: string;
+}
